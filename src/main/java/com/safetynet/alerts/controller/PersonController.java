f9ebde24d23566670dd597735b150f0378a4c7e9
etienne-verraest <verraest.etienne@gmail.com>
2022-03-31 18:35:16 +0200

    Added PersonController test class

--- src/main/java/com/safetynet/alerts/controller/PersonController.java
+++ src/main/java/com/safetynet/alerts/controller/PersonController.java
@@ -11,6 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.safetynet.alerts.model.Person;
 import com.safetynet.alerts.service.PersonService;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 @RequestMapping("person")
 public class PersonController {
@@ -20,11 +23,13 @@ public class PersonController {
 
 	@GetMapping
 	public Iterable<Person> returnPeopleJson() {
+		log.info("Fetching people from database...");
 		return personService.getPeopleFromDatabase();
 	}
 
 	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
 	public Person createPerson(@RequestBody Person person) {
+		log.info("Adding {} {} to database", person.getFirstName(), person.getLastName());
 		return personService.createPerson(person);
 	}
 }
---
+++ src/test/java/com/safetynet/alerts/controller/PersonControllerTest.java
@@ -0,0 +1,30 @@
+package com.safetynet.alerts.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.safetynet.alerts.service.PersonService;
+
+@WebMvcTest(controllers = PersonController.class)
+public class PersonControllerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@MockBean
+	private PersonService personService;
+
+	@Test
+	@DisplayName("GET on /person should return a 200 status")
+	public void performGet_ShouldReturnStatus200() throws Exception {
+		mockMvc.perform(get("/person")).andExpect(status().isOk());
+	}
+
+}
