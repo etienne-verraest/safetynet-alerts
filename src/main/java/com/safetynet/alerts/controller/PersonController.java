0d6439b8b0e64e19b802d2ec6e3a141dfd22e0bc
etienne-verraest <verraest.etienne@gmail.com>
2022-03-31 10:42:49 +0200

    Created RestController for Person

--- src/main/java/com/safetynet/alerts/controller/PersonController.java
+++ src/main/java/com/safetynet/alerts/controller/PersonController.java
@@ -1,8 +1,20 @@
 package com.safetynet.alerts.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.safetynet.alerts.service.PersonService;
 
 @RestController
 public class PersonController {
+
+	@Autowired
+	PersonService personService;
+
+	@GetMapping("/person")
+	public void getPeople() {
+		// TODO : Implements get Person
+	}
 
 }
--- src/main/java/com/safetynet/alerts/service/PersonService.java
+++ src/main/java/com/safetynet/alerts/service/PersonService.java
@@ -1,8 +1,19 @@
 package com.safetynet.alerts.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.safetynet.alerts.model.Person;
+import com.safetynet.alerts.repository.PersonRepository;
 
 @Service
 public class PersonService {
+
+	@Autowired
+	PersonRepository personRepository;
+
+	public Iterable<Person> getPersons() {
+		return personRepository.findAll();
+	}
 
 }
