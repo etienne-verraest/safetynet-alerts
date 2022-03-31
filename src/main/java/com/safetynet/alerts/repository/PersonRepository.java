2887f98a6a6906ad1c13ffff93d8915b49f99f9a
etienne-verraest <verraest.etienne@gmail.com>
2022-03-31 10:02:42 +0200

    Implemented PersonRepository

--- src/main/java/com/safetynet/alerts/repository/PersonRepository.java
+++ src/main/java/com/safetynet/alerts/repository/PersonRepository.java
@@ -1,8 +1,11 @@
 package com.safetynet.alerts.repository;
 
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import com.safetynet.alerts.model.Person;
+
 @Repository
-public class PersonRepository {
+public interface PersonRepository extends CrudRepository<Person, Integer> {
 
 }
