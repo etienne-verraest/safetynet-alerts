03864de574125f910220217f48b9bc4311694c77
etienne-verraest <verraest.etienne@gmail.com>
2022-04-18 22:47:00 +0200

    Added MedicalRecordDto and PostMapping for MedicalRecordController

--- src/main/java/com/safetynet/alerts/controller/MedicalRecordController.java
+++ src/main/java/com/safetynet/alerts/controller/MedicalRecordController.java
@@ -4,10 +4,10 @@ import java.util.List;
 
 import javax.transaction.Transactional;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,9 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.safetynet.alerts.exception.ExceptionMessages;
 import com.safetynet.alerts.exception.ResourceNotFoundException;
+import com.safetynet.alerts.mapper.PersonId;
 import com.safetynet.alerts.model.Allergy;
+import com.safetynet.alerts.model.Medication;
 import com.safetynet.alerts.model.Person;
-import com.safetynet.alerts.model.dto.AllergyDto;
+import com.safetynet.alerts.model.dto.MedicalRecordDto;
+import com.safetynet.alerts.model.dto.MedicationDto;
 import com.safetynet.alerts.service.PersonService;
 
 import lombok.extern.slf4j.Slf4j;
@@ -31,49 +34,64 @@ public class MedicalRecordController {
 	@Autowired
 	PersonService personService;
 
+	@Autowired
+	ModelMapper modelMapper;
+
 	/**
-	 * This method adds a new allergy to a person medical record 
+	 * This method adds a new medical record to a person
 	 * 
-	 * @param firstName		The first name of the person
-	 * @param lastName		The last name of the person
-	 * @param allergyDto	{@link AllergyDto.java}
+	 * @param firstName     The first name of the person
+	 * @param lastName      The last name of the person
+	 * @param medicationDto {@link MedicationDto.java}
 	 * @return updated information about a person
 	 * @throws ResourceNotFoundException if the person was not found in database
 	 */
-	@PostMapping("/{firstName}/{lastName}/allergy")
-	public ResponseEntity<Person> addAllergy(@PathVariable("firstName") String firstName,
-			@PathVariable("lastName") String lastName, @RequestBody AllergyDto allergyDto) {
+	@PostMapping
+	public ResponseEntity<Person> addMedicalRecord(@RequestBody MedicalRecordDto medicalRecordDto) {
+
+		String firstName = medicalRecordDto.getId().getFirstName();
+		String lastName = medicalRecordDto.getId().getLastName();
 
 		// Checking if the person exists in database
 		Person person = personService.getPersonFromDatabase(firstName, lastName);
 		if (person != null) {
 
-			// Getting person's allergy
+			PersonId personId = person.getId();
+
 			List<Allergy> personAllergies = person.getAllergies();
+			List<Medication> personMedications = person.getMedications();
 
-			// It is a POST request, it means that we are adding a new allergy
-			Allergy allergy = new Allergy();
-			allergy.setName(allergyDto.getName());
-			allergy.setPerson(person);
-			
-			personAllergies.add(allergy);
+			// Mapping Allergy Dto to Person's entity
+			medicalRecordDto.getAllergies().forEach(allergyDto -> {
+				allergyDto.setPerson(personId);
+				personAllergies.add(modelMapper.map(allergyDto, Allergy.class));
+			});
+
+			// Mapping Medication Dto to Person's entity
+			medicalRecordDto.getMedications().forEach(medicationDto -> {
+				medicationDto.setPerson(personId);
+				personMedications.add(modelMapper.map(medicationDto, Medication.class));
+			});
+
+			// Adding allergies and medications to Person entity
 			person.setAllergies(personAllergies);
+			person.setMedications(personMedications);
 
-			// Updating person's information
+			// Updating person's entity
 			personService.updatePerson(person);
-			
-			// Logging the request
-			log.info("[POST /MEDICALRECORD] Adding allergy '{}' to {} {}'s medical record", allergyDto.getName(), firstName, lastName);
+
+			// Logging our request
+			log.info("[POST /MEDICALRECORD] Added Medical record for {} {}", firstName, lastName);
 
 			// Returning person entity with medicalRecord information updated
 			return new ResponseEntity<Person>(person, HttpStatus.ACCEPTED);
 		}
-		
+
 		// Logging the error
-		log.error("[PUT /FIRESTATION] Person with name '{} {}' was not found in database",
-				firstName, lastName);
-		
+		log.error("[POST /MEDICALRECORD] Person with name '{} {}' was not found in database", firstName, lastName);
+
 		// Throwing an exception if the person doesn't exist
 		throw new ResourceNotFoundException(ExceptionMessages.PERSON_NOT_FOUND);
 	}
+
 }
--- src/main/java/com/safetynet/alerts/model/dto/AllergyDto.java
+++ src/main/java/com/safetynet/alerts/model/dto/AllergyDto.java
@@ -1,10 +1,18 @@
 package com.safetynet.alerts.model.dto;
 
+import com.safetynet.alerts.mapper.PersonId;
+
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AllergyDto {
-
-	private String name;
 	
+	private PersonId person;
+	
+	private String name;
+
 }
