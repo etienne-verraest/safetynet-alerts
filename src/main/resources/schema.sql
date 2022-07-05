a631e17ec3052bc9c74f421436d8e2d6bfb7d580
etienne-verraest <verraest.etienne@gmail.com>
2022-07-05 18:21:00 +0200

    Updated schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,41 +1,100 @@
-CREATE SCHEMA safetynet;
+-- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
+--
+-- Host: localhost    Database: safetynet
+-- ------------------------------------------------------
+-- Server version	8.0.29
 
-CREATE TABLE firestation ( 
-	station_id           INT      ,
-	address              VARCHAR(100)      
- ) engine=InnoDB;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-CREATE TABLE person ( 
-	firstname            VARCHAR(100)  NOT NULL    ,
-	lastname             VARCHAR(100)  NOT NULL    ,
-	address              VARCHAR(100)      ,
-	city                 VARCHAR(100)      ,
-	zip                  VARCHAR(100)      ,
-	phone                VARCHAR(100)      ,
-	email                VARCHAR(100)      ,
-	birthdate            VARCHAR(100)      ,
-	CONSTRAINT pk_person PRIMARY KEY ( firstname, lastname )
- ) engine=InnoDB;
+--
+-- Table structure for table `allergy`
+--
 
-CREATE TABLE allergy ( 
-	allergy_id           INT  NOT NULL  AUTO_INCREMENT  PRIMARY KEY,
-	name                 VARCHAR(100)      ,
-	p_firstname          VARCHAR(100)      ,
-	p_lastname           VARCHAR(100)      
- ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+DROP TABLE IF EXISTS `allergy`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `allergy` (
+  `allergy_id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) DEFAULT NULL,
+  `firstName` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `lastName` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  PRIMARY KEY (`allergy_id`),
+  KEY `fk_allergy_person` (`firstName`,`lastName`),
+  CONSTRAINT `fk_allergy_person` FOREIGN KEY (`firstName`, `lastName`) REFERENCES `person` (`firstName`, `lastName`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE medication ( 
-	medication_id        INT  NOT NULL  AUTO_INCREMENT  PRIMARY KEY,
-	name_posology        VARCHAR(100)      ,
-	p_firstname          VARCHAR(100)      ,
-	p_lastname           VARCHAR(100)      
- ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+--
+-- Table structure for table `firestation`
+--
 
-ALTER TABLE allergy ADD CONSTRAINT fk_allergy_person FOREIGN KEY ( p_firstname, p_lastname ) REFERENCES person( firstname, lastname ) ON DELETE NO ACTION ON UPDATE NO ACTION;
+DROP TABLE IF EXISTS `firestation`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `firestation` (
+  `stationNumber` int DEFAULT NULL,
+  `address` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-ALTER TABLE medication ADD CONSTRAINT fk_medication_person FOREIGN KEY ( p_firstname, p_lastname ) REFERENCES person( firstname, lastname ) ON DELETE NO ACTION ON UPDATE NO ACTION;
+--
+-- Table structure for table `medication`
+--
 
-ALTER TABLE person MODIFY firstname VARCHAR(100)  NOT NULL   COMMENT 'First name of the person, used for primary key';
+DROP TABLE IF EXISTS `medication`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `medication` (
+  `medication_id` int NOT NULL AUTO_INCREMENT,
+  `name_posology` varchar(100) DEFAULT NULL,
+  `firstName` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `lastName` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  PRIMARY KEY (`medication_id`),
+  KEY `fk_medication_person` (`firstName`,`lastName`),
+  CONSTRAINT `fk_medication_person` FOREIGN KEY (`firstName`, `lastName`) REFERENCES `person` (`firstName`, `lastName`)
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-ALTER TABLE person MODIFY lastname VARCHAR(100)  NOT NULL   COMMENT 'Last name of the person, used for primary key';
+--
+-- Table structure for table `person`
+--
 
+DROP TABLE IF EXISTS `person`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `person` (
+  `firstName` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT 'First name of the person, used for primary key',
+  `lastName` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT 'Last name of the person, used for primary key',
+  `address` varchar(100) DEFAULT NULL,
+  `city` varchar(100) DEFAULT NULL,
+  `zip` varchar(100) DEFAULT NULL,
+  `phone` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `birthdate` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`firstName`,`lastName`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping routines for database 'safetynet'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-07-05 18:06:54
