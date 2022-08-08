03e570da68609108db05941fe257fa48689c7860
etienne-verraest <verraest.etienne@gmail.com>
2022-08-08 16:20:30 +0200

    datapopulator under fix

--- src/main/java/com/safetynet/alerts/util/AgeCalculator.java
+++ src/main/java/com/safetynet/alerts/util/AgeCalculator.java
@@ -7,18 +7,19 @@ import java.time.format.DateTimeFormatter;
 public class AgeCalculator {
 
 	public static final String BIRTHDATE_FORMAT = "MM/dd/yyyy";
-	
-	private AgeCalculator() {}
-	
+
+	private AgeCalculator() {
+	}
+
 	public static Integer calculateAge(String birthdate) {
-		
+
 		// Parse birthdate to the correct formatting
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern(BIRTHDATE_FORMAT);
 		LocalDate from = LocalDate.parse(birthdate, formatter);
-	
+
 		// Calculates the time elapsed between 2 dates
 		Period age = Period.between(from, LocalDate.now());
 		return age.getYears();
 	}
-	
+
 }
